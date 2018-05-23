67c99228b77aec77cfed323f64cbcbe8417f1e32
chethanank <chethana.nk@globallogic.com>
2018-05-23 15:46:31 +0530

    swagger integration

--- src/main/java/com/gl/config/Config.java
+++ src/main/java/com/gl/config/Config.java
@@ -12,6 +12,16 @@ import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.IOException;
 
 /**
  * Created by chethana.nk on 29/3/18.
@@ -22,6 +32,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableAutoConfiguration
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class Config {
 
 
@@ -55,6 +66,16 @@ public class Config {
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
 
         return mongoTemplate;
+
+    }
+
+    @Bean
+    public Docket api() throws IOException, XmlPullParserException {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gl.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfo("Product management Service Api Documentation", "Documentation automatically generated", "1.1", null, new Contact("chethana", "localhost", "chethana.nk@globallogic.com"), null, null));
 
     }
 
